ead8b2419c43e838b4d5c73542867a3959e9867c
Gurkan420 <august.ljungdell@skola.botkyrka.se>
2020-09-28 15:52:34 +0200

    HEJ

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ document.getElementById("button").onclick = click
 function click (){
     alert("Bra jobbat, du hittade knappen")
 }
+function click() {
+document.getElementById("button").style.marginTop = "100px"
+}
